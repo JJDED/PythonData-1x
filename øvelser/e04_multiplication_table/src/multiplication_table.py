f5e1cbbf0717d7f0f3aec76e965349867fd662bc
JJDED <jamiedelasencerie@gmail.com>
2024-10-21 12:42:39 +0200

    Commit 3, 4 og 5

--- øvelser/e04_multiplication_table/src/multiplication_table.py
+++ øvelser/e04_multiplication_table/src/multiplication_table.py
@@ -2,7 +2,11 @@
 
 
 def main():
-    pass
+    for x in range(1, 11):      # Vandret
+        for y in range (1, 11): # Lodret
+            print('{:4d}'.format(x*y), end = "") # Sammy
+            #print(f"{x*y:4d}", end = "") # Søren
+        print()
 
 if __name__ == "__main__":
     main()
