f3971926e9c49841744cc7767e08ebfcad4c4676
JJDED <jamiedelasencerie@gmail.com>
2024-10-23 09:30:19 +0200

    OPG 15, 16 & 17

--- øvelser/e15_two_dice_comprehension/src/two_dice_comprehension.py
+++ øvelser/e15_two_dice_comprehension/src/two_dice_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    res5 = [(d1, d2) for d1 in range(1, 7) for d2 in range(1, 7) if d1+d2 == 5]
+    print(*res5, sep="\n")
 
 if __name__ == "__main__":
     main()
--- øvelser/e16_transform/src/transform.py
+++ øvelser/e16_transform/src/transform.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 def transform(s1, s2):
-    return []
+    listS1 = s1.split()
+    listS2 = s2.split()
+    z = zip(map(int, listS1), map(int, listS2))
+    return [i*j for i,j in z]
 
 def main():
+    print(transform("1  5 3", "2 6 -1"))
     pass
 
 if __name__ == "__main__":
